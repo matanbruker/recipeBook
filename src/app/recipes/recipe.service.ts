87430bffcf063141e3f791bc65e85c9a2b508650
matan <matanbruker7@gmail.com>
2020-08-12 00:20:49 +0300

    using recipe.service.ts to communicate between components in the recipe folder instead of using a lot of events

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('name', 'test', 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/t_MD_MainArticleFaceDetect/497741')];
+  recipeSelected = new EventEmitter<Recipe>();
 
   getRecipes() {
     return this.recipes.slice();
