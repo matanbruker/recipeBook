cbc969ee360e30793ba5ddc70ad483af503053b8
matan <matanbruker7@gmail.com>
2020-08-12 01:56:17 +0300

    display recipe's ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,12 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('name', 'test', 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/t_MD_MainArticleFaceDetect/497741')];
+    new Recipe('name', 'test', 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/t_MD_MainArticleFaceDetect/497741',
+      [new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)])];
   recipeSelected = new EventEmitter<Recipe>();
 
   getRecipes() {
