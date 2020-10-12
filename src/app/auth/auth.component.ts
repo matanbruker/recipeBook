e653033ab348e50c290640958a4a1d8528b0018f
matan <matanbruker7@gmail.com>
2020-10-13 00:55:58 +0300

    adding loading animation and handling sign up email exists error

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,43 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+  loginMode = true;
+  isLoading = false;
+  error: string = null;
 
+  constructor(private authService: AuthService) {
+  }
+
+  onSwitchMode() {
+    this.loginMode = !this.loginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+
+    if (this.loginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false;
+        }, errorMessage => {
+          this.error = errorMessage;
+          console.log(errorMessage);
+          this.isLoading = false;
+        }
+      );
+    }
+
+    form.reset();
+  }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+
+interface AuthResponse {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) {
+  }
+
+  signup(userEmail: string, userPassword: string) {
+    return this.http.post<AuthResponse>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBZhM9RnVlylRoQjvfyAexlmTZIIw-1f44',
+      {
+        email: userEmail,
+        password: userPassword,
+        returnSecureToken: true
+      }).pipe(catchError(errorRes => {
+        let errorMessage = 'An unknown error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
+    }));
+  }
+
+}
